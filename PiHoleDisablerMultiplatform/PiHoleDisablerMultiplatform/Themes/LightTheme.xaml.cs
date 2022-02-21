117825076d779f4192c5cee8e748b3ecdff9bfd9
Alberto Robledo <arobledo707@gmail.com>
2022-02-20 22:57:10 -0800

    theme changes

---
+++ PiHoleDisablerMultiplatform/PiHoleDisablerMultiplatform/Themes/LightTheme.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace PiHoleDisablerMultiplatform 
+{
+    public partial class LightTheme : ResourceDictionary
+    {
+        public LightTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
