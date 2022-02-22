5261e3c9f4634b38d428c43643c1c8180eda4079
Alberto Robledo <arobledo707@gmail.com>
2022-02-21 19:22:31 -0800

    theme switching with os done

--- PiHoleDisablerMultiplatform/PiHoleDisablerMultiplatform/App.xaml.cs
+++ PiHoleDisablerMultiplatform/PiHoleDisablerMultiplatform/App.xaml.cs
@@ -3,6 +3,7 @@ using PiHoleDisablerMultiplatform.Views;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
 
 namespace PiHoleDisablerMultiplatform
 {
@@ -12,9 +13,32 @@ namespace PiHoleDisablerMultiplatform
         public App()
         {
             InitializeComponent();
-            
+            Application.Current.RequestedThemeChanged += (OnThemeChange);
+
             DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
+        }
+
+        private void OnThemeChange(Object sender, AppThemeChangedEventArgs a) 
+        {
+            OSAppTheme theme = a.RequestedTheme;
+
+            ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+            if (mergedDictionaries != null)
+            {
+                mergedDictionaries.Clear();
+
+                switch (theme)
+                {
+                    case OSAppTheme.Dark:
+                        mergedDictionaries.Add(new DarkTheme());
+                        break;
+                    case OSAppTheme.Light:
+                    default:
+                        mergedDictionaries.Add(new LightTheme());
+                        break;
+                }
+            }
         }
 
         protected override void OnStart()
@@ -27,6 +51,7 @@ namespace PiHoleDisablerMultiplatform
 
         protected override void OnResume()
         {
+            //OSAppTheme currentTheme = Application.Current.RequestedTheme;
         }
     }
 }
