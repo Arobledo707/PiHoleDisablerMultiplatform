c546952bff5effb877ebdfa36e873d6902fc0b56
Alberto Robledo <arobledo707@gmail.com>
2022-03-07 22:16:40 -0800

    updated name and version

--- PiHoleDisablerMultiplatform/PiHoleDisablerMultiplatform.Android/MainActivity.cs
+++ PiHoleDisablerMultiplatform/PiHoleDisablerMultiplatform.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using ZXing.Mobile;
 
 namespace PiHoleDisablerMultiplatform.Droid
 {
-    [Activity(Label = "PiHole Disabler", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Pi-hole Disabler", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
