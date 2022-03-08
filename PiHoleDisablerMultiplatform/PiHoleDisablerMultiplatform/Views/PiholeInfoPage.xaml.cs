8100ab974a17c53398ca687e6aa753ad7e06c4e2
Alberto Robledo <arobledo707@gmail.com>
2022-03-07 20:58:30 -0800

    updated

--- PiHoleDisablerMultiplatform/PiHoleDisablerMultiplatform/Views/PiholeInfoPage.xaml.cs
+++ PiHoleDisablerMultiplatform/PiHoleDisablerMultiplatform/Views/PiholeInfoPage.xaml.cs
@@ -59,7 +59,7 @@ namespace PiHoleDisablerMultiplatform.Views
             base.OnAppearing();
             if (NeedsPiholeData())
             {
-                PiholeHttp.CheckPiholeStatus("pi.hole", "fakeToken");
+                PiholeHttp.CheckPiholeStatus(piholeAddress.Placeholder.Trim(), "fakeToken");
                 MessagingCenter.Send(this, Commands.infoRequest);
             }
         }
