84d8a80ce8b71f78224936cc9e9a97e93ad8bc80
Alberto Robledo <arobledo707@gmail.com>
2022-03-13 17:39:59 -0700

    layouit testing

--- PiHoleDisablerMultiplatform/PiHoleDisablerMultiplatform/ViewModels/QueryViewModel.cs
+++ PiHoleDisablerMultiplatform/PiHoleDisablerMultiplatform/ViewModels/QueryViewModel.cs
@@ -4,14 +4,25 @@ using System.Text;
 using PiHoleDisablerMultiplatform.Services;
 using PiHoleDisablerMultiplatform.StaticPi;
 using PiHoleDisablerMultiplatform.Models;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 using Newtonsoft.Json;
 
 namespace PiHoleDisablerMultiplatform.ViewModels
 {
     public class QueryViewModel : BaseViewModel
     {
+        private bool isRefreshing = false;
+        public bool IsCurrentlyRefreshing 
+        {
+            get => isRefreshing;
+            set => SetProperty(ref isRefreshing, value);
+        }
+        public Command RefreshCommand { get;}
+
         public QueryViewModel()
         {
+            RefreshCommand = new Command(Refresh);
             Title = "Queries";
             Test();
         }
@@ -25,6 +36,38 @@ namespace PiHoleDisablerMultiplatform.ViewModels
             }
             QueryData data = JsonConvert.DeserializeObject<QueryData>(contentString);
 
+        }
+
+        private async void Refresh() 
+        {
+            //IsCurrentlyRefreshing = true;
+            StackLayout stackLayout = CreateStackLayout(Color.LightGreen);
+
+            string contentString = await PiholeHttp.GetQueries(CurrentPiData.piHoleData.Url, CurrentPiData.piHoleData.Token, 10);
+            if (contentString == String.Empty || contentString == null)
+            {
+                return;
+            }
+            QueryData data = JsonConvert.DeserializeObject<QueryData>(contentString);
+            IsCurrentlyRefreshing = false;
+        }
+
+        private StackLayout CreateStackLayout(Color color) 
+        {
+            StackLayout stackLayout = new StackLayout();
+            stackLayout.Orientation = StackOrientation.Horizontal;
+            stackLayout.HorizontalOptions = LayoutOptions.FillAndExpand;
+            stackLayout.FlowDirection = FlowDirection.LeftToRight;
+            stackLayout.BackgroundColor = color;
+
+            return stackLayout;
+        }
+
+        private void AttachItems(ref StackLayout layout) 
+        {
+            Label label = new Label();
+            label.Text = "lol";
+            //layout.Children.Add();
         }
     }
 }
